1c0a9d94821d203b51c187ecb49015e9085b093c
DamonCZH <2535180928@qq.com>
2018-04-13 09:28:19 +0800

    Add files via upload
    只含有一个隐藏层的神经网络，该层使用RBM提取特征，训练准确率为10%

---
+++ rbm.py
@@ -0,0 +1,37 @@
+import tensorflow as tf 
+from tensorflow.examples.tutorials.mnist import input_data
+from tfrbm import BBRBM
+import layer
+
+mnist = input_data.read_data_sets("D:/MNIST/MNIST_data",one_hot=True)
+
+learning_rate = 0.01
+
+x = tf.placeholder(shape=[None,784],dtype=tf.float32)
+y = tf.placeholder(shape=[None,10],dtype=tf.float32)
+
+r = layer.train_rbm(784,200)
+hidden_layer = layer.rbm_layer(x,r,"hidden_layer")
+output_layer = layer.nn_layer(hidden_layer,200,10,"output_layer",tf.nn.softmax)
+
+pred = output_layer
+loss = -tf.reduce_sum(y*tf.log(pred))
+update = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(pred,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    
+    train_values = {
+        x:mnist.train.images,
+        y:mnist.train.labels
+    }
+    sess.run(update,feed_dict=train_values)
+
+    test_values = {
+        x:mnist.test.images,
+        y:mnist.test.labels
+    }
+    print(sess.run(accuracy,feed_dict=test_values))
