9217284f135d5a4d83ef7ba83bd9b13829e7ab81
DamonCZH <2535180928@qq.com>
2018-04-13 09:25:56 +0800

    Add files via upload
    包括两个构造网络层的函数，nn_layer和rbm_layer

---
+++ layer.py
@@ -0,0 +1,22 @@
+import tensorflow as tf 
+from tensorflow.examples.tutorials.mnist import input_data
+from tfrbm import BBRBM
+
+mnist = input_data.read_data_sets("D:/MNIST/MNIST_data",one_hot=True)
+
+def nn_layer(input_tensor,input_dim,output_dim,layer_name,activation):
+	W = tf.Variable(tf.truncated_normal([input_dim,output_dim],stddev=0.1))
+	b = tf.Variable(tf.constant(0.1,shape=[output_dim]))
+	output = activation(tf.add(tf.matmul(input_tensor,W),b))
+	return output
+
+def rbm_layer(input_tensor,rbm,layer_name,activation=tf.nn.sigmoid):
+    W,a,b = rbm.get_weights()
+    output = activation(tf.add(tf.matmul(input_tensor,W),b))
+    return output
+
+
+def train_rbm(n_vis, n_hid):
+    rbm = BBRBM(n_vis, n_hid, learning_rate=0.01, momentum=0.95, use_tqdm=True)
+    rbm.fit(mnist.train.images, n_epoches=1, batch_size=100)
+    return rbm
